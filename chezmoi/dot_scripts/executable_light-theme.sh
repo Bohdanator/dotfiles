52702948d8ea9f93f92237f91f1e139780dc55ba
David Misiak <misiakdavid@gmail.com>
2023-10-13 18:23:34 +0200

    Add theme toggle

--- chezmoi/dot_scripts/executable_light-theme.sh
+++ chezmoi/dot_scripts/executable_light-theme.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-if [ "$1" == "0" ]; then
+INPUT="$1"
+
+if [ "$INPUT" == "t" ]; then
+    if [[ $(gsettings get org.gnome.desktop.interface gtk-theme) =~ [Dd]ark ]]; then
+        INPUT="1"
+    else
+        INPUT="0"
+    fi
+fi
+
+if [ "$INPUT" == "0" ]; then
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
     gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
     gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
     sed -i -e 's/"workbench.colorTheme": ".*"/"workbench.colorTheme": "Default Dark Modern"/g' "$HOME/.config/Code/User/settings.json"
 fi
 
-if [ "$1" == "1" ]; then
+if [ "$INPUT" == "1" ]; then
     gsettings set org.gnome.desktop.interface color-scheme prefer-light
     gsettings set org.gnome.desktop.interface gtk-theme 'Arc'
     gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
