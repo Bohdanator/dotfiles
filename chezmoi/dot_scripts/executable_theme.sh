6c4e0f0911dbd5c18a7dded1ebb1e730228816ec
David Misiak <misiakdavid@gmail.com>
2024-05-21 00:06:28 +0200

    Improve theme switching

--- chezmoi/dot_scripts/executable_theme.sh
+++ chezmoi/dot_scripts/executable_theme.sh
@@ -15,6 +15,10 @@ if [ "$INPUT" == "0" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
     gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
     sed -i -e 's/"workbench.colorTheme": ".*"/"workbench.colorTheme": "Default Dark Modern"/g' "$HOME/.config/Code/User/settings.json"
+    sed -i -e 's/.*theme-.*/"~\/.config\/alacritty\/theme-dark.toml",/g' "$HOME/.config/alacritty/alacritty.toml"
+    sed -i -e 's/colors=.*/colors=colors-dark/g' "$HOME/.config/wofi/config"
+    sed -i -e 's/@import "theme-.*";/@import "theme-dark.css";/g' "$HOME/.config/waybar/style.css"
+    swaymsg reload
 fi
 
 if [ "$INPUT" == "1" ]; then
@@ -22,4 +26,8 @@ if [ "$INPUT" == "1" ]; then
     gsettings set org.gnome.desktop.interface gtk-theme 'Arc'
     gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
     sed -i -e 's/"workbench.colorTheme": ".*"/"workbench.colorTheme": "Default Light Modern"/g' "$HOME/.config/Code/User/settings.json"
+    sed -i -e 's/.*theme-.*/"~\/.config\/alacritty\/theme-light.toml",/g' "$HOME/.config/alacritty/alacritty.toml"
+    sed -i -e 's/colors=.*/colors=colors-light/g' "$HOME/.config/wofi/config"
+    sed -i -e 's/@import "theme-.*";/@import "theme-light.css";/g' "$HOME/.config/waybar/style.css"
+    swaymsg reload
 fi
